f6a52be7fa1074983502fb7cabc12879b47d124b
Kyesha <franciskyesha1@gmail.com>
2019-07-29 15:45:09 -0400

    deleted yarn, using npm

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React { Component } from 'react'
 
-class Home extends React. Component {
-  render() {
-    return(
-      <p>No One gets Left Behind</p>
-    )
-  }
+class Home extends Component {
+    constructor() {
+        super()
+    }
 
 
+    render() {
+        const imageStyle = {
+            width: 400
+        }
+        return (
+            <div>
+                <p>It's good to be home</p>
+                <img style={imageStyle} src="https://i.ytimg.com/vi/N1icEHtgb3g/maxresdefault.jpg" />
+            </div>
+        )
+
+    }
 }
 
-export default Home;
+export default Home
