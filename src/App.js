2b6c5b4b1c7d22a865e1626e3b1abda0ee36c248
afrikabrownlee <afrikabrownlee@gmail.com>
2019-07-29 15:17:51 -0400

    navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './App.css';
+import './index.js';
 
 
-class App extends React.Component{
+// Event
+class App extends Component {
   render() {
-    return(
-      
-    )
-  }
+    return (
+      // Event Container
+      <div className="container">
+      </div>
+      // Ened Event container
 
+    );
+  }
+}
+// End Event
 
 export default App;
