a61fcd022a4bf951fb8c84400c33ee87f432a9b5
Kyesha <franciskyesha1@gmail.com>
2019-08-07 12:04:54 -0400

    fixed ports and merge conflicts

--- src/App.js
+++ src/App.js
@@ -1,65 +1,10 @@
 import React from 'react';
-<<<<<<< HEAD
-import axios from 'axios';
-import Home from './Components/Home';
-// import Navbar from './Components/Navbar';
-import { BrowserRouter, Route} from "react-router-dom";
-import Event from './Components/Event';
-import './App.css';
-import './index.js';
-import './Components/Event';
-// import './index.html';
-
-//
-const API_KEY = process.env.REACT_APP_OHANA_API_KEY;
-
-class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       events: [] ,
-//       images: []
-//     };
-//   }
-//
-//   componentDidMount = () => {
-//     const showMeSomething = axios.get('/ohana', { headers: { Authorization: 'Bearer ' + API_KEY }})
-//     .then((response) => {
-//       console.log(response)
-//       this.setState({events: response.data})
-//
-//     })
-//
-//
-//     const showMeImage = axios.get('/ohana', { headers: { Authorization: 'Bearer ' + API_KEY }})
-//     .then((response) => {
-//       console.log(response.data)
-//       this.setState({image:response.data})
-//     })
-//
-//     }
-
-render() {
-    return (
-        <Event/>
-        <Profile/>
-    )
-  }
-};
-
-//     render() {
-//       return (
-//
-//         <Event/>
-//         // <Profile/>
-//         )};
-// }
-=======
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import TryNavbar from "./Components/TryNavbar";
 import SignUp from "./Components/SignUp";
+import Event from "./Components/Event";
 
 import axios from 'axios';
 import './App.css';
@@ -75,6 +20,8 @@ class App extends React.Component {
     return(
       <Router className="App">
                 <TryNavbar/>
+                <Route path="/Event" to={Event}></Route>
+                <Event />
                 <Route path="/Home" to={Home}></Route>
                 <Home />
                 <Route path="/Profile" to={Profile}></Route>
@@ -86,6 +33,5 @@ class App extends React.Component {
   }
 }
 
->>>>>>> bf9fa2ae4e23eb3fee04a88daf73e775b1752067
 
 export default App;
